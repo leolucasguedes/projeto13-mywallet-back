825b73dce5d6b166e276ec6af97ba5bc852fbc13
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-05-09 15:36:24 -0300

    Controllers/Crypt

---
+++ controllers/signUpController.js
@@ -0,0 +1,45 @@
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import { v4 as uuid } from "uuid";
+import bcrypt from "bcrypt";
+
+import database from "../database.js";
+
+export async function postSignUp(req, res) {
+  const { name, email, password, confirmation } = req.body;
+  const registerschema = joi.object({
+    name: joi.string().min(3).max(30).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(1).required(),
+    repassword: joi.ref("password"),
+  });
+  const xx = { name, email, password, confirmation };
+  const { error } = registerschema.validate(xx, { abortEarly: false });
+  const cryptPassword = bcrypt.hashSync(password, 10);
+  if (error) {
+    chalk.bold.red(console.log(error));
+    res.status(409).send(
+      validate.error.details.map((e) => {
+        return e.message;
+      })
+    );
+    return;
+  }
+  const user = { name, email, password: cryptPassword };
+
+  try {
+    const registeredUser = await database
+      .collection("users")
+      .findOne({ email });
+    if (registeredUser) {
+      return res.status(400).send("E-mail já cadastrado. Escolha outro!");
+    }
+    await database
+    .collection("users")
+    .insertOne(user);
+    res.sendStatus(200);
+  } catch (e) {
+    chalk.bold.red(console.log(e));
+    res.status(409).send(e);
+  }
+}
--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import { v4 as uuid } from "uuid";
+import { v4 as uuid} from "uuid";
 import joi from "joi";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br.js";
+
+import database from "./database.js";
+import { postSignUp } from "./controllers/signUpController.js";
+import { postSignIn } from "./controllers/signInController.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-const token = uuid();
 app.use(cors());
 app.use(json());
 
@@ -17,130 +22,75 @@ app.listen(port, () => {
   console.log(chalk.bold.green(`Server is running on: ${port}`));
 });
 
-let database = null;
-const mongoclient = new MongoClient(process.env.MONGO_URL);
-const promise = mongoclient.connect();
-promise.then(() => {
-  database = mongoclient.db(process.env.DATABASE);
-  console.log(chalk.bold.green("Connected to database"));
-});
-promise.catch((e) =>
-  console.log(chalk.bold.red("Error to connected to database", e))
-);
+app.post("/signup", postSignUp);
 
-app.post("/login", async (req, res) => {
-  const user = req.body;
-  const loginschema = joi.object({
-    email: joi.string().pattern(/@/).required(),
-    password: joi.required(),
+app.post("/", postSignIn);
+
+app.post("/myrecords", async (req, res) => {
+  const { authorization } = req.headers;
+  const { value, type, describe } = req.body;
+  const validateschema = joi.object({
+    value: joi.number().min(1).required(),
+    type: joi.any().valid("entrada", "saida").required(),
+    describe: joi.string().min(1).required(),
   });
-  const { error } = loginschema.validate(req.body);
+  const { error } = validateschema.validate(req.body);
   if (error) {
-    chalk.bold.red(console.log(error));
-    res.sendStatus(422);
+    res.status(400).send(
+      objectValidate.error.details.map((e) => {
+        return e.message;
+      })
+    );
     return;
   }
+  const valid = authorization?.replace("bearer", "").trim();
+  if (!valid) return res.sendStatus(401);
   try {
-    const registeredUser = await database.collection("users").findOne({
-      email: user.email,
-      password: user.password,
-    });
-    if (registeredUser) {
-      return res.sendStatus(201);    
+    const search = await database
+      .collection("sessions")
+      .findOne({ token: valid });
+    const registeredUser = await database
+      .collection("users")
+      .findOne({ _id: search.userId });
+    if (!registeredUser) return res.sendStatus(404);
+    const account = {
+      value,
+      type,
+      describe,
+      time: dayjs().format("DD/MM"),
+      user: registeredUser._id,
+    };
+    await database.collection("records").insertOne(account);
+    res.status(201).send(account);
+  } catch (e) {
+    chalk.bold.red(console.log(e));
+    res.sendStatus(409);
+  }
+});
+
+app.get("/myrecords", async (req, res) => {
+  const { authorization } = req.headers;
+  const valid = authorization?.replace("Bearer ", "");
+  if (!valid) return res.sendStatus(401);
+  try {
+    const search = await database.collection("sessions").findOne({ valid });
+    if (!search) return res.sendStatus(401);
+
+    const user = await database
+      .collection("users")
+      .findOne({ _id: search.userId });
+    if (user) {
+      const balance = await database
+        .collection("records")
+        .find({ userId: user._id })
+        .toArray();
+      if (!balance) return res.sendStatus(401);
+      res.status(200).send(balance);
+    } else {
+      res.sendStatus(404);
     }
   } catch (e) {
     chalk.bold.red(console.log(e));
-    return res.status(500).send("Usuário não encontrado");
-  }
-});
-
-app.post("/cadastro", async (req, res) => {
-  const user = req.body;
-  const registerschema = joi.object({
-    name: joi.string().min(3).required(),
-    email: joi.string().pattern(/@/).required(),
-    password: joi.required(),
-  });
-  const { error } = registerschema.validate(req.body);
-  if (error) {
-    chalk.bold.red(console.log(error));
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    const registeredUser = await database.collection("users").findOne({
-      email: user.email,
-      password: user.password,
-    });
-    if (registeredUser) {
-      return res.sendStatus(409);
-    }
-    await database.collection("users").insertOne({
-      name: user.name,
-      email: user.email,
-      password: user.password,
-    });
-    res.sendStatus(201);
-  } catch (e) {
-    chalk.bold.red(console.log(e));
-    return res.status(500).send("Erro ao cadastrar o usuário");
-  }
-});
-
-app.get("/registros", async (req, res) => {
-  try {
-    const records = await database.collection("registros").find().toArray();
-    res.send(records);
-  } catch (e) {
-    console.log(e);
-    return res.status(500).send("Erro ao obter os registros!", e);
-  }
-});
-
-app.post("/novaentrada", async (req, res) => {
-  const entry = req.body;
-  const entryschema = joi.object({
-    value: joi.number().required(),
-    description: joi.string().required(),
-  });
-  const { error } = entryschema.validate(req.body);
-  if (error) {
-    chalk.bold.red(console.log(error));
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    await database.collection("registros").insertOne({
-      value: user.value,
-      description: user.description,
-    });
-    res.sendStatus(201);
-  } catch (e) {
-    chalk.bold.red(console.log(e));
-    return res.sendStatus(500);
-  }
-});
-
-app.post("/novasaida", async (req, res) => {
-  const exit = req.body;
-  const exitschema = joi.object({
-    value: joi.number().required(),
-    description: joi.string().required(),
-  });
-  const { error } = exitschema.validate(req.body);
-  if (error) {
-    chalk.bold.red(console.log(error));
-    res.sendStatus(422);
-    return;
-  }
-  try {
-    await database.collection("registros").insertOne({
-      value: user.value,
-      description: user.description,
-    });
-    res.sendStatus(201);
-  } catch (e) {
-    chalk.bold.red(console.log(e));
-    return res.sendStatus(500);
+    res.sendStatus(500);
   }
 });
